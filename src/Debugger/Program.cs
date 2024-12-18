0550ed0b0f457ad7044b0b389d2a014c5c361cd4
stefanhall <stefan-hall@hotmail.com>
2024-12-18 13:23:08 +1300

    Day 9 first try

--- src/Debugger/Program.cs
+++ src/Debugger/Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Helpers;
-using Solutions.Day12;
+using Solutions.Day9;
 
-var basicInput = FilePathHelper.GetFullFilePath("12_basic.txt");
-var fullInputFile = FilePathHelper.GetFullFilePath("12_full.txt");
+var basicInput = FilePathHelper.GetFullFilePath("9_basic.txt");
+var fullInputFile = FilePathHelper.GetFullFilePath("9_full.txt");
 
 
-var basicSolution = Day12Solution.LoadSolution(basicInput);
-var fullSolution = Day12Solution.LoadSolution(fullInputFile);
-Console.WriteLine($"---{nameof(Day12Solution)}-basic-part-1---");
+var basicSolution = Day9Solution.LoadSolution(basicInput);
+var fullSolution = Day9Solution.LoadSolution(fullInputFile);
+Console.WriteLine($"---{nameof(Day9Solution)}-basic-part-1---");
 Console.WriteLine(basicSolution.SolvePart1());
-Console.WriteLine($"---{nameof(Day12Solution)}-full-part-1---");
-Console.WriteLine(fullSolution.SolvePart1());
+// Console.WriteLine($"---{nameof(Day9Solution)}-full-part-1---");
+// Console.WriteLine(fullSolution.SolvePart1());
 //
 // Console.WriteLine("");
 // Console.WriteLine($"---{nameof(Day12Solution)}-basic-part-2---");
--- src/Solutions/Day9/Day9Solution.cs
+++ src/Solutions/Day9/Day9Solution.cs
@@ -1,6 +1,112 @@
+using Helpers.Solution;
+using OneOf;
+
 namespace Solutions.Day9;
 
-public class Day9Solution
+public enum BlockType
+{
+    Space,
+    File
+}
+
+public record Block(long Size, BlockType Type)
+{
+};
+
+public record FileBlock(long Id, long Size) : Block(Size, BlockType.File)
+{
+    public string Debug => new(Id.ToString()[0], (int)Size);
+};
+
+public record SpaceBlock(long Size) : Block(Size, BlockType.Space)
+{
+    public string Debug => new('.', (int)Size);
+};
+
+public class OneOfBlock : OneOfBase<FileBlock, SpaceBlock>
+{
+    protected OneOfBlock(OneOf<FileBlock, SpaceBlock> input) : base(input)
+    {
+        
+    }
+
+    public bool IsFile => IsT0;
+    public bool IsSpace => IsT1;
+
+    public string Debug()
+    {
+        return IsFile ? AsT0.Debug : AsT1.Debug;
+    }
+    
+    public static implicit operator OneOfBlock(FileBlock input) => new(input);
+    public static implicit operator OneOfBlock(SpaceBlock spaceBlock) => new(spaceBlock);
+}
+
+
+public class Day9Solution(LinkedList<OneOfBlock> blocks) : ISolution
 {
     
+    public long SolvePart1()
+    {
+        
+        return 1;
+    }
+
+    private LinkedListNode<OneOfBlock> NextEmptySpace(LinkedListNode<OneOfBlock> node)
+    {
+        while (node.Value.IsFile && node.Next != null)
+        {
+            node = node.Next;
+        }
+
+        return node;
+    }
+    
+    private LinkedListNode<OneOfBlock> LastFullSpace(LinkedListNode<OneOfBlock> node)
+    {
+        while (node!.Value.IsSpace && node.Next != null)
+        {
+            node = node.Previous!;
+        }
+
+        return node;
+    }
+
+    public long SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Day9Solution LoadSolution(string basicInput)
+    {
+        var numbers = File.ReadAllLines(basicInput)
+            .First().Trim();
+        
+        var input = new LinkedList<OneOfBlock>();
+        var blockType = BlockType.File;
+        long fileId = 0;
+        foreach (var number in numbers)
+        {
+            var size = long.Parse(number.ToString());
+            if (blockType == BlockType.File)
+            {
+                OneOfBlock block = new FileBlock(fileId, size);
+                
+                input.AddLast(block);
+                fileId++;
+            }
+            else
+            {
+                OneOfBlock block = new SpaceBlock(size);
+                input.AddLast(block);
+            }
+            blockType = Toggle(blockType);
+        }
+        return new Day9Solution(input);
+    }
+
+    private static BlockType Toggle(BlockType blockType)
+    {
+        return blockType == BlockType.File ? BlockType.Space : BlockType.File;
+    }
 }
